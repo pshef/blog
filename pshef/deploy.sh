865c52c12be1cd79f95be36a73583bd6f6b1056f
pshef <pshef@pm.me>
2022-02-06 16:44:02 -0600

    rebuilding site Sun Feb  6 04:44:02 PM CST 2022

--- pshef/deploy.sh
+++ pshef/deploy.sh
@@ -5,32 +5,31 @@ echo -e "Deploying updates to GitHub..."
 # Build project
 hugo -t eureka
 
-# Add changes to git
-git add .
-
-msg="rebuilding site 'date'"
-if [ $# -eq 1 ]
+# Add commit message
+msg="rebuilding site `date`"
+if [$# -eq 1]
 	then msg="$1"
 fi
 
+# Add changes to root git
+git add .
+
+# Commit root changes
 git commit -m "$msg"
 
+# Push root changes
 git push origin main
 
 # Changing to Public Folder
-
 cd public
 
-# Adding Public changes to git
+# Adding /public changes
 git add .
 
-msg="rebuilding site 'date'"
-if [ $# -eq 1 ]
-        then msg="$1"
-fi
-
+# Commit /public changes
 git commit -m "$msg"
 
+# Push /public changes
 git push origin main
 
 # Go back to Project Root
